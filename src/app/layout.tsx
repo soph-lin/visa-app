cf97dca4ea0449868ea589ebcd89291a8605af93
Sophie Lin <98501518+soph-lin@users.noreply.github.com>
2025-07-01 16:42:52 +0800

    Update redirect url for signin and signup buttons which fixes signin issues hopefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,16 @@ export default function RootLayout({
         <body className={`${inter.variable} ${inter.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
-              <SignUpButton>
+              <SignInButton
+                forceRedirectUrl="/onboarding"
+                fallbackRedirectUrl="/onboarding"
+                mode="modal"
+              />
+              <SignUpButton
+                forceRedirectUrl="/onboarding"
+                fallbackRedirectUrl="/onboarding"
+                mode="modal"
+              >
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
